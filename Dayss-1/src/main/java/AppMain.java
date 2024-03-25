18daf27d592444b67d6009ad2c114cee998618c9
Hakan ÖZER <hakanozer02@gmail.com>
2024-03-25 16:24:00 +0300

    Days-1

--- Dayss-1/src/main/java/AppMain.java
+++ Dayss-1/src/main/java/AppMain.java
@@ -1,17 +1,38 @@
 import entities.Customer;
 import services.CustomerService;
 
+import java.util.List;
+
 public class AppMain {
     public static void main(String[] args) {
 
         CustomerService service = new CustomerService();
         Customer customer = new Customer();
-        customer.setName("Erkan");
-        customer.setEmail("erkan@mail.com");
+        customer.setName("Ali");
+        customer.setEmail("ali@mail.com");
         customer.setPassword("12345");
-        customer.setStatus(true);
+        customer.setStatus(false);
 
         customer = service.save(customer);
-        System.out.println(customer);
+        if (customer != null) {
+            System.out.println("Insert Success");
+            System.out.println(customer);
+        }else {
+            System.out.println("Insert Fail");
+        }
+
+        //  Select list
+        List<Customer> ls = service.allCustomer(false);
+        for (Customer item : ls) {
+            System.out.println(item);
+        }
+
+        System.out.println("================================");
+        // Like list
+        List<Customer> lsx = service.customerSearch("mail", 2, 2);
+        for (Customer item: lsx) {
+            System.out.println(item);
+        }
+
     }
 }
--- Dayss-1/src/main/java/services/CustomerService.java
+++ Dayss-1/src/main/java/services/CustomerService.java
@@ -6,6 +6,8 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 
+import java.util.List;
+
 public class CustomerService {
 
     SessionFactory db1 = DB1HibernateUtil.getSessionFactory();
@@ -13,12 +15,41 @@ public class CustomerService {
     public Customer save(Customer customer) {
         Session session = db1.openSession();
         Transaction tr = session.beginTransaction();
-        // insert into customer (id,name,email,password,status) values(default,?,?,?,?)
-        session.persist(customer);
-        // Customer c = (Customer) session.save(customer);
-        tr.commit();
-        session.close();
+
+        try {
+            session.persist(customer);
+            tr.commit();
+        }catch (Exception ex) {
+            tr.rollback();
+            customer = null;
+        }finally {
+            session.close();
+        }
+
         return customer;
+    }
+
+    public List<Customer> allCustomer(boolean status) {
+        Session session = db1.openSession();
+        List<Customer> ls =
+                session
+                .createQuery("from Customer c where c.status = ?1", Customer.class)
+                .setParameter(1, status)
+                .list();
+        return ls;
+    }
+
+    public List<Customer> customerSearch(String data, int start, int max) {
+        Session session = db1.openSession();
+        List<Customer> ls =
+                session
+                .createQuery("from Customer c where c.name like ?1 or c.email like ?2", Customer.class)
+                .setParameter(1, "%"+data+"%")
+                .setParameter(2, "%"+data+"%")
+                .setFirstResult(start)
+                .setMaxResults(max)
+                .list();
+        return ls;
     }
 
 }
