846464c73f26c3250d5858c862781ad62f60e8b9
Hakan ÖZER <hakanozer02@gmail.com>
2024-03-26 14:20:29 +0300

    2-Days

--- Dayss-1/src/main/java/AppMain.java
+++ Dayss-1/src/main/java/AppMain.java
@@ -7,6 +7,7 @@ public class AppMain {
     public static void main(String[] args) {
 
         CustomerService service = new CustomerService();
+        /*
         Customer customer = new Customer();
         customer.setName("Ali");
         customer.setEmail("ali@mail.com");
@@ -33,6 +34,50 @@ public class AppMain {
         for (Customer item: lsx) {
             System.out.println(item);
         }
+
+        // Login Customer
+        Customer cus = service.customerLogin("ali@mail.com", "12345");
+        if (cus != null) {
+            System.out.println(cus);
+        }else {
+            System.out.println("Email or password fail!");
+        }
+
+        //select c1_0.cid,c1_0.email,c1_0.name,c1_0.password,c1_0.status from Customer c1_0 where c1_0.email=? and c1_0.password=?
+
+        service.customerLoginColumn("ali@mail.com", "12345");
+
+        Customer updateCustomer = new Customer();
+        updateCustomer.setCid(7l);
+        updateCustomer.setName("Serkan");
+        updateCustomer.setEmail("serkan@mail.com");
+        updateCustomer.setStatus(true);
+        updateCustomer.setPassword("12345");
+        service.customerUpdate(updateCustomer);
+
+
+       Customer customer = service.customerFindEmailNameChange("serkan@mail.com", "Ali");
+       if (customer == null) {
+           System.out.println("Update Fail");
+       }else {
+           System.out.println("Update Success :" + customer);
+       }
+
+        // Delete
+        Customer customer1 = new Customer();
+        customer1.setCid(42l);
+        long status = service.customerDelete(customer1);
+        if (status > 0) {
+            System.out.println("Delete success: " + status);
+        }else {
+            System.out.println("Delete fail: " + customer1.getCid());
+        }
+        */
+        long start = System.currentTimeMillis();
+        service.allSave(1000000);
+        long end = System.currentTimeMillis();
+        long between = end - start;
+        System.out.println("between :" + between);
 
     }
 }
--- Dayss-1/src/main/java/services/CustomerService.java
+++ Dayss-1/src/main/java/services/CustomerService.java
@@ -7,6 +7,7 @@ import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 
 import java.util.List;
+import java.util.Objects;
 
 public class CustomerService {
 
@@ -51,5 +52,101 @@ public class CustomerService {
                 .list();
         return ls;
     }
+
+
+    public Customer customerLogin( String email, String password ) {
+        Session session = db1.openSession();
+        Customer customer = null;
+        try {
+            customer = (Customer) session
+                    .createQuery("from Customer c where c.email = ?1 and c.password = ?2")
+                    .setParameter(1, email)
+                    .setParameter(2, password)
+                    .getSingleResult();
+        }catch (Exception ex) { }
+        return customer;
+    }
+
+    public Object customerLoginColumn( String email, String password ) {
+        Session session = db1.openSession();
+        Object obj = null;
+        try {
+            obj = session
+                    .createQuery("select c.name, c.email from Customer c where c.email = ?1 and c.password = ?2")
+                    .setParameter(1, email)
+                    .setParameter(2, password)
+                    .getSingleResult();
+        }catch (Exception ex) { }
+        Object[] cus = (Object[]) obj;
+        for( Object item : cus ) {
+            System.out.println( item );
+        }
+        return obj;
+    }
+
+
+    public void customerUpdate( Customer customer ) {
+        Session session = db1.openSession();
+        Transaction tr = session.beginTransaction();
+        session.update(customer);
+        tr.commit();
+        session.close();
+    }
+
+    public Customer customerFindEmailNameChange( String email, String name ) {
+        Session session = db1.openSession();
+        Transaction tr = session.beginTransaction();
+        Customer customer = null;
+        try {
+            customer = session
+                   .createQuery("from Customer c where c.email = ?1", Customer.class)
+                   .setParameter(1, email)
+                   .getSingleResult();
+           customer.setName(name);
+           session.update(customer);
+           tr.commit();
+        }catch (Exception ex) {
+        }finally {
+            session.close();
+        }
+        return customer;
+    }
+
+    public long customerDelete(Customer customer) {
+        Session session = db1.openSession();
+        Transaction tr = session.beginTransaction();
+        long status = 0;
+        try {
+            session.remove(customer);
+            tr.commit();
+            status = customer.getCid();
+        }catch (Exception ex ){
+
+        }finally {
+            session.close();
+        }
+        return status;
+    }
+
+    public void allSave(int count) {
+        Session session = db1.openSession();
+        Transaction tr = session.beginTransaction();
+        try {
+            for (int i = 0; i < count; i++) {
+                Customer c = new Customer();
+                c.setPassword("12345");
+                c.setEmail("mail@mail"+i+".com");
+                c.setName("Name"+i);
+                c.setStatus(true);
+                session.persist(c);
+            }
+            tr.commit();
+        }catch (Exception  ex) {
+        }finally {
+            session.close();
+        }
+
+    }
+
 
 }
