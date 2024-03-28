30db45ea4ca8ba93da993763fd08066da81a4f2b
Hakan ÖZER <hakanozer02@gmail.com>
2024-03-28 13:47:51 +0300

    Update CustomerRestController.java

--- JPA/src/main/java/com/works/restcontrollers/CustomerRestController.java
+++ JPA/src/main/java/com/works/restcontrollers/CustomerRestController.java
@@ -1,7 +1,10 @@
 package com.works.restcontrollers;
 
+import com.works.entities.Customer;
 import com.works.services.CustomerService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,6 +13,9 @@ public class CustomerRestController {
 
     final CustomerService customerService;
 
-    
+    @PostMapping("customerAdd")
+    public Customer customerAdd(@RequestBody Customer customer) {
+        return customerService.customerAdd(customer);
+    }
 
 }
